3c990b80a8c66d587c36e0802a580b22ea1a48aa
ayseguldalkiran <aysegul.dalkiran@logo.com.tr>
2025-01-15 21:55:54 +0300

    kaç tane contract item geleceği contractList in size ı ile belirlendi

--- privacy-policy-lib/src/main/java/com/example/privacy_policy_lib/CheckBoxViewModel.kt
+++ privacy-policy-lib/src/main/java/com/example/privacy_policy_lib/CheckBoxViewModel.kt
@@ -4,11 +4,17 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CheckBoxViewModel : ViewModel() {
-    val checkboxStates = MutableLiveData(booleanArrayOf(false, false, false))
+    val checkboxStates = MutableLiveData<BooleanArray>()
+
+    fun initialize(size: Int) {
+        checkboxStates.value = BooleanArray(size) { false }
+    }
 
     fun setCheckboxState(index: Int, state: Boolean) {
-        val states = checkboxStates.value ?: BooleanArray(3)
-        states[index] = state
-        checkboxStates.value = states
+        val states = checkboxStates.value ?: BooleanArray(checkboxStates.value?.size ?: 0)
+        if (index in states.indices) {
+            states[index] = state
+            checkboxStates.value = states
+        }
     }
 }
