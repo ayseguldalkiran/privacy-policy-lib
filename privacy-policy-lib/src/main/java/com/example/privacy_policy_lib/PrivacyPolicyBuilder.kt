70d69bf7fda226f103aa20df0d906a6c95d3fd57
ayseguldalkiran <aysegul.dalkiran@logo.com.tr>
2025-01-21 00:11:40 +0300

    PrivacyPolicyBuilder java classlarından görünmüyordu, object yerine classa çevrildi

--- privacy-policy-lib/src/main/java/com/example/privacy_policy_lib/PrivacyPolicyBuilder.kt
+++ privacy-policy-lib/src/main/java/com/example/privacy_policy_lib/PrivacyPolicyBuilder.kt
@@ -3,17 +3,19 @@ package com.example.privacy_policy_lib
 import android.os.Bundle
 import com.example.privacy_policy_lib.core.model.PrivacyPolicyLibParams
 
-object PrivacyPolicyBuilder {
-    @JvmStatic
-    fun createContractsFragment(
-        params: PrivacyPolicyLibParams? = null,
-        onAccepted: (() -> Unit)? = null
-    ): ContractsFragment {
-        return ContractsFragment().apply {
-            arguments = Bundle().apply {
-                putParcelable("params", params)
+class PrivacyPolicyBuilder {
+    companion object {
+        @JvmStatic
+        fun createContractsFragment(
+            params: PrivacyPolicyLibParams? = null,
+            onAccepted: (() -> Unit)? = null
+        ): ContractsFragment {
+            return ContractsFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable("params", params)
+                }
+                onPrivacyPolicyAccepted = onAccepted
             }
-            onPrivacyPolicyAccepted = onAccepted
         }
     }
 } 
