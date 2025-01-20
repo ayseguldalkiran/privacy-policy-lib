7e9e5c8ceee39c4b67b56f6807b5ebe6b4ee3990
ayseguldalkiran <aysegul.dalkiran@logo.com.tr>
2025-01-21 00:12:30 +0300

    eski haline alındı

--- privacy-policy-lib/src/main/java/com/example/privacy_policy_lib/PrivacyPolicyBuilder.kt
+++ privacy-policy-lib/src/main/java/com/example/privacy_policy_lib/PrivacyPolicyBuilder.kt
@@ -3,19 +3,17 @@ package com.example.privacy_policy_lib
 import android.os.Bundle
 import com.example.privacy_policy_lib.core.model.PrivacyPolicyLibParams
 
-class PrivacyPolicyBuilder {
-    companion object {
-        @JvmStatic
-        fun createContractsFragment(
-            params: PrivacyPolicyLibParams? = null,
-            onAccepted: (() -> Unit)? = null
-        ): ContractsFragment {
-            return ContractsFragment().apply {
-                arguments = Bundle().apply {
-                    putParcelable("params", params)
-                }
-                onPrivacyPolicyAccepted = onAccepted
+object PrivacyPolicyBuilder {
+    @JvmStatic
+    fun createContractsFragment(
+        params: PrivacyPolicyLibParams? = null,
+        onAccepted: (() -> Unit)? = null
+    ): ContractsFragment {
+        return ContractsFragment().apply {
+            arguments = Bundle().apply {
+                putParcelable("params", params)
             }
+            onPrivacyPolicyAccepted = onAccepted
         }
     }
 } 
